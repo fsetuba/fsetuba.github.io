e409662b278b62ade64d838cb32f08c3035112e6
fsetuba <fabiosetuba@gmail.com>
2025-04-16 21:21:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 let display = document.getElementById("display");
+
+function adicionarValor(valor) {
+  display.value += valor;
+}
+
+function calcular() {
+  try{
+    display.value = eval(display.value);
+  }catch{
+    display.value = "Erro";
+  }
+}
